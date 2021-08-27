e4569e56f51261141bdfa5493eb5235d130f0eb8
imxnat <ngrm97@gmail.com>
2021-08-27 19:49:19 -0400

    :sparkles: new feat route file system
    new feat

---
+++ src/pages/blog/{mdx.slug}.js
@@ -0,0 +1,14 @@
+import * as React from "react";
+import Layout from "../../components/Layout";
+
+
+const BlogPostPage = () => {
+    return (
+        <Layout pageTitle="Da Posts">
+           <p>AQUI LOS POST</p>
+        </Layout>
+    )
+}
+
+export default BlogPostPage;
+
